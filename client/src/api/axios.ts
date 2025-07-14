e837431e33b713598609942d2778f02ce760acdd
kevin-njuguna <kevy.kamah@gmail.com>
2025-07-14 20:12:22 +0300

    update axios baseURL to production URL

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://blogit-86vo.onrender.com",
   withCredentials: true,
 });
 
